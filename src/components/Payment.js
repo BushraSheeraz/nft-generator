6f1b28563081f98694ac89632bb545c819e93724
Bushra Sheeraz <bushragemini14@gmail.com>
2021-10-14 14:53:00 +0500

    Update Payment.js

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -10,134 +10,136 @@ import { faCheck } from '@fortawesome/free-solid-svg-icons';
 function Payment() {
     return (
         <>
-            <Container className="payment">
+            <Container>
+                <div className="payment">
+                    <Card className="paymentCard" style={{ 
+                        background: 'linear-gradient(90deg, rgba(86,43,149,1) 0%, rgba(73,62,216,1) 100%)'
+                        }}>
+                        <Card.Body>
+                            <Table className="paymentCard-table" style={{ color: 'white' }}>
+                                <tr className="cardHeading">
+                                    <td>
+                                        <h2>Pro Plan</h2>
+                                    </td>
+                                    <td>
+                                        <h3>700$</h3>
+                                    </td>
+                                </tr>
+                                <tbody>
+                                    <tr>Plan includes</tr>
+                                    <tr>
+                                        <td>
+                                            <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
+                                            500 artworks
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
+                                            500 artworks
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
+                                            500 artworks
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
+                                            500 artworks
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </Table>
 
-                <Card className="paymentCard" style={{ background: 'linear-gradient(90deg, rgba(86,43,149,1) 0%, rgba(73,62,216,1) 100%)' }}>
+                            <button className="Lite"> Choose Lite Plan</button>
+                        </Card.Body>
+                    </Card>
+                    <Card className="paymentCard" style={{ background: '#EBEAFA', color: 'rgba(86,43,149,1)' }}>
+                        <Card.Body>
+                            <Table className="paymentCard-table" style={{ color: 'rgba(86,43,149,1)' }}>
+                                <tr className="cardHeading">
+                                    <td>
+                                        <h2>Lite</h2>
+                                    </td>
+                                    <td><h3>Free</h3></td>
+                                </tr>
+                                <tbody>
+                                    <tr>Plan includes</tr>
+                                    <tr className="list">
+                                        <td>
+                                            <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
+                                            500 artworks
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
+                                            500 artworks
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
+                                            500 artworks
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
+                                            500 artworks
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </Table>
 
-                    <Card.Body>
-                        <Table className="paymentCard-table" style={{ color: 'white' }}>
-                            <tr className="cardHeading">
-                                <td>
-                                    <h2>Pro Plan</h2>
-                                </td>
-                                <td>
-                                    <h3>700$</h3>
-                                </td>
-                            </tr>
-                            <tbody>
-                                <tr>Plan includes</tr>
-                                <tr>
+                            <button className="trial"> Choose Lite Plan</button>
+                        </Card.Body>
+                    </Card>
+                    <Card className="paymentCard" style={{ background: 'rgba(86,43,149,1)', color: 'white' }}>
+                        <Card.Body>
+                            <Table className="paymentCard-table" style={{ color: 'white' }}>
+                                <tr className="cardHeading">
                                     <td>
-                                        <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
-                                        500 artworks
+                                        <h2>Pro Plan</h2>
                                     </td>
+                                    <td><h3>300$</h3></td>
                                 </tr>
-                                <tr>
-                                    <td>
-                                        <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
-                                        500 artworks
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
-                                        500 artworks
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
-                                        500 artworks
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </Table>
+                                <tbody>
+                                    <tr>Plan includes</tr>
+                                    <tr>
+                                        <td>
+                                            <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
+                                            500 artworks
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
+                                            500 artworks
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
+                                            500 artworks
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
+                                            500 artworks
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </Table>
 
-                        <button className="Lite"> Choose Lite Plan</button>
-                    </Card.Body>
-                </Card>
-                <Card className="paymentCard" style={{ background: '#EBEAFA', color: 'rgba(86,43,149,1)' }}>
-                    <Card.Body>
-                        <Table className="paymentCard-table" style={{ color: 'rgba(86,43,149,1)' }}>
-                            <tr className="cardHeading">
-                                <td>
-                                    <h2>Lite</h2>
-                                </td>
-                                <td><h3>Free</h3></td>
-                            </tr>
-                            <tbody>
-                                <tr>Plan includes</tr>
-                                <tr className="list">
-                                    <td>
-                                        <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
-                                        500 artworks
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
-                                        500 artworks
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
-                                        500 artworks
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
-                                        500 artworks
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </Table>
-
-                        <button className="trial"> Choose Lite Plan</button>
-                    </Card.Body>
-                </Card>
-                <Card className="paymentCard" style={{ background: 'rgba(86,43,149,1)', color: 'white' }}>
-                    <Card.Body>
-                        <Table className="paymentCard-table" style={{ color: 'white' }}>
-                            <tr className="cardHeading">
-                                <td>
-                                    <h2>Pro Plan</h2>
-                                </td>
-                                <td><h3>300$</h3></td>
-                            </tr>
-                            <tbody>
-                                <tr>Plan includes</tr>
-                                <tr>
-                                    <td>
-                                        <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
-                                        500 artworks
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
-                                        500 artworks
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
-                                        500 artworks
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <FontAwesomeIcon icon={faCheck} style={{ marginRight: '10px' }} />
-                                        500 artworks
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </Table>
-
-                        <button className="Lite"> Choose Lite Plan</button>
-                    </Card.Body>
-                </Card>
+                            <button className="Lite"> Choose Lite Plan</button>
+                        </Card.Body>
+                    </Card>
+                </div>
             </Container>
         </>
     )
